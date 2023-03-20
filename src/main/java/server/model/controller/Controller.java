4250dd3ebd8883286a51f96c2e346c495b57744c
andrebricchi <andrea2.bricchi@mail.polimi.it>
2023-03-20 19:03:31 +0100

    Add files via upload

---
+++ src/main/java/server/model/controller/Controller.java
@@ -0,0 +1,24 @@
+package server.model.Controller
+
+
+public class Controller {
+
+
+    public void extractAndRestore(){
+    }
+
+    public selectTiles(){
+    }
+
+    public void insertIntoShelf(BookShelf s){
+    }
+
+    public void endGame(){
+    }
+
+    public void setScore(){
+    }
+
+    public void updateCommonCards(){
+    }
+}
