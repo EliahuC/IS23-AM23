a1ace9251b014794f6d7ce34a7bf93de434d9642
andrebricchi <andrea2.bricchi@mail.polimi.it>
2023-04-02 18:39:46 +0200

    Update BookShelf.java

--- src/main/java/server/model/player/BookShelf.java
+++ src/main/java/server/model/player/BookShelf.java
@@ -4,9 +4,13 @@ import server.model.board.ItemTile;
 public class BookShelf {
 
     private ItemTile[][] Shelf;
+    private int counter;
+    private int points;
 
     public BookShelf() {
         this.Shelf =  new ItemTile[6][5];
+        this.counter = 1;
+        this.points = 0;
     }
 
     public ItemTile getTile(int x , int y) {
@@ -14,5 +18,17 @@ public class BookShelf {
     }
 
     public void AdjacentScore(){}
+    
+    public void SetPoints(){
+        if(counter > 2){
+            switch (counter){
+                case 3 : points += 2;
+                case 4 : points += 3;
+                case 5 : points += 5;
+                default : points += 8;
+            }
+        }
+    }
+
 
 }
