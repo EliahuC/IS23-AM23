faeeb182b301a6be769fa134e6e18b565a1d1b3c
GiovanniDiLorenzo <126008664+GiovanniDiLorenzo@users.noreply.github.com>
2023-04-03 15:55:18 +0200

    Add files via upload

--- src/main/java/server/model/board/goalCards/CommonGoalCard9.java
+++ src/main/java/server/model/board/goalCards/CommonGoalCard9.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class CommonGoalCard9 extends CommonGoalCard implements checkCommonGoalCard {
     private List<ItemTile> validGroups;
     private static int numColumnsToAchieve=2;
-    private List<itemTileCategory> cat = new ArrayList<>(); //I use an hashset to track the categories in each column
+    private List<itemTileCategory> cat = new ArrayList<>(); //I use an arrayList to track the categories in each column
                                                                 //of the bookshelf
     @Override
     public void checkGoal(BookShelf bs) {
