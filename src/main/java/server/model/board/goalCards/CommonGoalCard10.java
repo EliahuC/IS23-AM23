c5961692c18079c51aeb22901f197db78d902e54
GiovanniDiLorenzo <126008664+GiovanniDiLorenzo@users.noreply.github.com>
2023-04-04 08:42:42 +0200

    Add files via upload

--- src/main/java/server/model/board/goalCards/CommonGoalCard10.java
+++ src/main/java/server/model/board/goalCards/CommonGoalCard10.java
@@ -1,12 +1,29 @@
-package server.model.board.goalCards;
+package server.model.board;
 import server.model.board.ItemTile;
 import server.model.player.BookShelf;
 
+import java.util.ArrayList;
 import java.util.List;
-
-public class CommonGoalCard10 extends CommonGoalCard implements checkCommonGoalCard{
+public class CommonGoalCard10 extends CommonGoalCard implements checkCommonGoalCard {
     private List<ItemTile> validGroups;
+    private final static int numRowsToAchieve=2;
+    private final List<itemTileCategory> cat = new ArrayList<>(); //I use an arrayList to track the categories in each row
+                                                                //of the bookshelf
     @Override
     public void checkGoal(BookShelf bs) {
+        int counter=0;
+        boolean SameCategory=false;
+        for(int i=0; i<bs.getRows() && counter<numRowsToAchieve; i++){
+            cat.add(bs.getTile(i,0).getCategory());
+            for(int j=1; j<bs.getColumns() && !SameCategory; j++){
+                if(cat.contains(bs.getTile(i,j).getCategory()))
+                    SameCategory=true;
+            }
+            if (!SameCategory)
+                counter++;
+            cat.clear();
+        }
+        if (counter==numRowsToAchieve)
+            increaseNumCompleted();
     }
 }
