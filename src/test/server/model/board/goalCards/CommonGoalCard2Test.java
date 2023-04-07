5a7251bdeb0a8bedd95b828e98f1c30763b476d4
GiovanniDiLorenzo <126008664+GiovanniDiLorenzo@users.noreply.github.com>
2023-04-07 12:46:26 +0200

    Add files via upload

---
+++ src/test/server/model/board/goalCards/CommonGoalCard2Test.java
@@ -0,0 +1,40 @@
+package server.model;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import server.model.board.*;
+import server.model.player.BookShelf;
+
+/**
+ * Unit test for simple App.
+ */
+public class CommonGoalCard2Test
+        extends TestCase
+{
+    public void testCheckGoalCardCGC2_FIRST() {
+        CommonGoalCard c6 = new CommonGoalCard2();
+        c6.initNumCompleted();
+        BookShelf bs = new BookShelf();
+        bs.setTile(0,0,new ItemTile("CATS"));
+        bs.setTile(0,4,new ItemTile("CATS"));
+        bs.setTile(5,0,new ItemTile("CATS"));
+        bs.setTile(5,4,new ItemTile("CATS"));
+        c6.checkGoal(bs);
+        assertEquals(1,c6.getNumCompleted());
+    }
+
+    public void testCheckGoalCardCGC2_SECOND() {
+        CommonGoalCard c7 = new CommonGoalCard2();
+        c7.initNumCompleted();
+        BookShelf bs = new BookShelf();
+        bs.setTile(0,0,new ItemTile("CATS"));
+        bs.setTile(0,4,new ItemTile("CATS"));
+        bs.setTile(5,0,new ItemTile("CATS"));
+        bs.setTile(5,4,new ItemTile("TROPHIES"));
+        c7.checkGoal(bs);
+        assertEquals(0, c7.getNumCompleted());
+    }
+
+}
+
