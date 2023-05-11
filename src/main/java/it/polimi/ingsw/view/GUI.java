5bb1eae6b4ad4bd37f7d0e900f9aad750ddda172
andrebricchi <andrea2.bricchi@mail.polimi.it>
2023-05-11 19:55:05 +0200

    GUI

--- src/main/java/it/polimi/ingsw/view/GUI.java
+++ src/main/java/it/polimi/ingsw/view/GUI.java
@@ -1,8 +1,50 @@
 package it.polimi.ingsw.view;
+/*
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
-public class GUI extends View{
-    @Override
-    public void run() {
+import java.io.IOException;
 
+public class Main extends Application{
+    Stage window;
+    Scene scene1, scene2;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws IOException {
+        window = primaryStage;
+
+        Label label1 = new Label("Welcome!");
+        Button button1 = new Button("Login");
+        button1.setOnAction(e -> window.setScene(scene2));
+
+        VBox layout1 = new VBox(100);
+        layout1.setAlignment(Pos.CENTER);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 600,600);
+
+        Button button2 = new Button("Main Menu");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        window.setScene(scene1);
+        window.setTitle("My Shelfie");
+        window.show();
+
+        // Image Icon = new Image("Icon 50x50px.png");
+        //window.getIcons().add(Icon);
+        //window.setFullScreen(true);
     }
 }
+*/
