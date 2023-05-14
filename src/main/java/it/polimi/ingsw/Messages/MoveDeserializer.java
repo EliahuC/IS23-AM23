3b4c652af7ba9ad30ba309d534b0d735ead45806
Eliahu Cohen <126007216+EliahuC@users.noreply.github.com>
2023-05-14 18:51:54 +0200

    MoveDeserializer

--- src/main/java/it/polimi/ingsw/Messages/MoveDeserializer.java
+++ src/main/java/it/polimi/ingsw/Messages/MoveDeserializer.java
@@ -12,62 +12,62 @@ public class MoveDeserializer {
         JsonObject o=new JsonParser().parse(s).getAsJsonObject();
         String category=o.get("messageCategory").getAsString();
         Gson gson=new Gson();
-        switch (category){
-            case "PINGFROMSERVER":{
-                return gson.fromJson(s,PingFromServer.class);
-            }
-            case "COORDINATES":{
-               return gson.fromJson(s, CoordinatesMessage.class) ;
-            }
-            case "COLUMN":{
-                return gson.fromJson(s, ColumnMessage.class);
-            }
-            case "ORDER":{
-                return gson.fromJson(s, OrderMessage.class);
-            }
-            case "BOOKSHELF":{
-                return gson.fromJson(s, BookshelfMessage.class);
-            }
-            case "LIVINGROOM":{
-                return gson.fromJson(s, LivingRoomMessage.class);
-            }
-            case "VALID_MESSAGE":{
-                return gson.fromJson(s, ValidMoveMessage.class);
-            }
-            case "STARTING_GAME_MESSAGE":{
-                return gson.fromJson(s, GameIsStartingMessage.class);
-            }
-            case "LAST_TURN_MESSAGE":{
-                return gson.fromJson(s, LastTurnMessage.class);
-            }
-            case "CREATE_LOBBY":{
-                return gson.fromJson(s, LobbyCreationMessage.class);
-            }
-            case "SUCCESS":{
-                return gson.fromJson(s, SuccessMessage.class);
-            }
-            case "START_GAME":{
-                return gson.fromJson(s, StartGameMessage.class);
-            }
-            case "ENTER_LOBBY":{
-                return gson.fromJson(s, LobbyEntranceMessage.class);
-            }
-            case "LOGOUT_LOBBY":{
-                return gson.fromJson(s, LobbyLogoutMessage.class);
-            }
-            case "END_GAME_MESSAGE":{
-                return gson.fromJson(s, EndGameMessage.class);
-            }
-            case "WARNING":{
-                return gson.fromJson(s, ErrorMessage.class);
-            }
-            case "NICKNAME":{
-                return gson.fromJson(s, NickNameMessage.class);
-            }
-            case "VALID_NICKNAME":{
-                return gson.fromJson(s, ValidNicknameMessage.class);
-            }
-        }
+       switch (category) {
+           case "PINGFROMSERVER" -> {
+               return gson.fromJson(s, PingFromServer.class);
+           }
+           case "COORDINATES" -> {
+               return gson.fromJson(s, CoordinatesMessage.class);
+           }
+           case "COLUMN" -> {
+               return gson.fromJson(s, ColumnMessage.class);
+           }
+           case "ORDER" -> {
+               return gson.fromJson(s, OrderMessage.class);
+           }
+           case "BOOKSHELF" -> {
+               return gson.fromJson(s, BookshelfMessage.class);
+           }
+           case "LIVINGROOM" -> {
+               return gson.fromJson(s, LivingRoomMessage.class);
+           }
+           case "VALID_MESSAGE" -> {
+               return gson.fromJson(s, ValidMoveMessage.class);
+           }
+           case "STARTING_GAME_MESSAGE" -> {
+               return gson.fromJson(s, GameIsStartingMessage.class);
+           }
+           case "LAST_TURN_MESSAGE" -> {
+               return gson.fromJson(s, LastTurnMessage.class);
+           }
+           case "CREATE_LOBBY" -> {
+               return gson.fromJson(s, LobbyCreationMessage.class);
+           }
+           case "SUCCESS" -> {
+               return gson.fromJson(s, SuccessMessage.class);
+           }
+           case "START_GAME" -> {
+               return gson.fromJson(s, StartGameMessage.class);
+           }
+           case "ENTER_LOBBY" -> {
+               return gson.fromJson(s, LobbyEntranceMessage.class);
+           }
+           case "LOGOUT_LOBBY" -> {
+               return gson.fromJson(s, LobbyLogoutMessage.class);
+           }
+           case "END_GAME_MESSAGE" -> {
+               return gson.fromJson(s, EndGameMessage.class);
+           }
+           case "WARNING" -> {
+               return gson.fromJson(s, ErrorMessage.class);
+           }
+           case "NICKNAME" -> {
+               return gson.fromJson(s, NickNameMessage.class);
+           }
+           case "VALID_NICKNAME" -> {
+               return gson.fromJson(s, ValidNicknameMessage.class);
+           }
+       }
         return null;
     }
 
