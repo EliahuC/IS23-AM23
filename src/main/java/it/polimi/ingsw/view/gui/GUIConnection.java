b5dd1f33c8d677385f1e875f067c3e812c95353d
andrebricchi <andrea2.bricchi@mail.polimi.it>
2023-05-20 18:52:04 +0200

    GUI changes

---
+++ src/main/java/it/polimi/ingsw/view/gui/GUIConnection.java
@@ -0,0 +1,42 @@
+/*package it.polimi.ingsw.view.gui;
+
+import it.polimi.ingsw.Messages.ServerToClient.ServerMessage;
+import it.polimi.ingsw.Network.Client.ConnectionClient;
+import it.polimi.ingsw.Network.Client.RMI.ClientConnectionRMI;
+import it.polimi.ingsw.Network.Client.TCP.ClientConnectionTCP;
+import it.polimi.ingsw.view.cli.CLIEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import it.polimi.ingsw.Settings;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.rmi.RemoteException;
+
+public class GUIConnection {
+
+    private String nickname;
+    private String serverAddr;
+    private int portNum;
+    private Socket socket;
+    private ConnectionClient connectionClient;
+
+    public boolean startTCP{
+
+
+        try {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    public boolean startRMI{
+            try {
+
+            } catch (RemoteException e){
+                e.printStackTrace();
+    }
+}
+
+*/
