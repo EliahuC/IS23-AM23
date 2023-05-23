c0a29b02eda1dde89e31b59bc96517d30e14d8f4
andrebricchi <andrea2.bricchi@mail.polimi.it>
2023-05-23 10:18:10 +0200

    GUI changes

---
+++ src/main/java/it/polimi/ingsw/view/gui/MenuController.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.view.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.io.IOException;
+
+public class MenuController {
+
+    @FXML
+    TextField textField;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+
+        String nickname = textField.getText();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("lobby.fxml"));
+        root = loader.load();
+
+        LobbyWaitingController lobbyController = loader.getController();
+        lobbyController.displayNickname(nickname);
+
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
