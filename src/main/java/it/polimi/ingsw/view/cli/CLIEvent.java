6e70e150b9ebb2d752b326e8f59c12e7f7792361
Eliahu Cohen <126007216+EliahuC@users.noreply.github.com>
2023-05-25 10:16:54 +0200

    un po' di ordine

--- src/main/java/it/polimi/ingsw/view/cli/CLIEvent.java
+++ src/main/java/it/polimi/ingsw/view/cli/CLIEvent.java
@@ -31,28 +31,35 @@ public class CLIEvent implements PropertyChangeListener {
         ServerMessage serverMessage=(ServerMessage) evt.getNewValue();
         if(gameHandler!=null){
             switch (serverMessage.getCategory()) {
-                case STARTING_GAME_MESSAGE:
-                    GameIsStartingMessage temp_startingGameMessage= (GameIsStartingMessage) serverMessage;
+                case STARTING_GAME_MESSAGE: {
+                    GameIsStartingMessage temp_startingGameMessage = (GameIsStartingMessage) serverMessage;
                     gameHandler.setPlayers(temp_startingGameMessage.getPlayers());
                     gameHandler.setLivingRoom(temp_startingGameMessage.getLivingRoom());
                     gameHandler.setPlayer(temp_startingGameMessage.getPlayers().stream().filter(player -> Objects.equals(player.getNickName(), gameHandler.getConnectionClient().getPlayerName())).findFirst().orElseThrow(() -> new IllegalArgumentException("Player not found")));
                     gameHandler.setCurrentPlayer(temp_startingGameMessage.getCurrPlaying());
                     break;
-                case UPDATE_STATE:
+                }
+                case UPDATE_STATE:{
                     UpdateStateMessage temp_updateStateMessage=(UpdateStateMessage) serverMessage;
                     gameHandler.setLivingRoom(temp_updateStateMessage.getGame().getLivingRoom());
                     gameHandler.setPlayers(temp_updateStateMessage.getGame().getPlayers());
                     gameHandler.setPlayer(temp_updateStateMessage.getGame().getPlayers().stream().filter(player -> Objects.equals(player.getNickName(), gameHandler.getConnectionClient().getPlayerName())).findFirst().orElseThrow(() -> new IllegalArgumentException("Player not found")));
                     gameHandler.setCurrPlaying(temp_updateStateMessage.getGame().getWhoIsPlaying());
                     break;
-                case CURRPLAYING:
+                }
+                case CURRPLAYING:{
                     CurrPlayingMessage temp_currPlayingMessage=(CurrPlayingMessage) serverMessage;
                     gameHandler.setCurrPlaying(temp_currPlayingMessage.getCurrPlaying());
                     break;
-                case END_GAME_MESSAGE:
+                }
+                case END_GAME_MESSAGE:{
                     EndGameMessage temp_endGameMessage = (EndGameMessage) serverMessage;
                     gameHandler.setWinner(temp_endGameMessage.getWinner().getNickName());
                     break;
+                }
+                case WARNING: {
+                    System.out.println(serverMessage.getReturnMessage());
+                }
             }
         }
         forwardMessage(serverMessage);
