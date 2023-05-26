1a5bc858a9cf5a92bb81d16d46d9144feaeb5c45
andrebricchi <andrea2.bricchi@mail.polimi.it>
2023-05-26 19:27:28 +0200

    GUI changes

--- src/main/java/it/polimi/ingsw/view/gui/GameControllerGUI.java
+++ src/main/java/it/polimi/ingsw/view/gui/GameControllerGUI.java
@@ -49,15 +49,7 @@ public class GameControllerGUI {
         stage.show();
     }
 
-    public void goToGoals(ActionEvent event) throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("goals.fxml"));
-        root = loader.load();
-        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
     /*
     public void displayLivingroom{
 
