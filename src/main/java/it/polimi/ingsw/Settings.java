4402d9ab607c5ed870bd669fd6b422b68bf4d0ec
Eliahu Cohen <126007216+EliahuC@users.noreply.github.com>
2023-06-06 10:28:41 +0200

    fix

--- src/main/java/it/polimi/ingsw/Settings.java
+++ src/main/java/it/polimi/ingsw/Settings.java
@@ -6,5 +6,5 @@ package it.polimi.ingsw;
  */
 public class Settings {
     public static int PORT = 2201;
-    public static String SERVER_NAME = "172.29.90.1";
+    public static String SERVER_NAME = "127.0.0.1";
 }
