4b50d6021a940da3f16be80cefd8f9e897282be2
Eliahu Cohen <126007216+EliahuC@users.noreply.github.com>
2023-06-28 17:39:47 +0200

    javadoc

--- deliverables/JavaDoc/search.js
+++ deliverables/JavaDoc/search.js
@@ -356,8 +356,8 @@ $.widget("custom.catcomplete", $.ui.autocomplete, {
             }
             li.attr("class", "result-item");
         });
-        ul.append("" + pathtoroot + "<li class='ui-static-link'><a href='search.html?q='>Go to search page</a></li>"
-            + encodeURI(widget.term) + "");
+        ul.append("<li class='ui-static-link'><a href='" + pathtoroot + "search.html?q="
+            + encodeURI(widget.term) + "'>Go to search page</a></li>");
     },
     _renderItem: function(ul, item) {
         var li = $("<li/>").appendTo(ul);
@@ -410,7 +410,7 @@ $(function() {
     $("section[id] > :header, :header[id], :header:has(a[id])").hover(
         function () {
             $(this).append($("<button class='copy copy-header' onclick='copyUrl(this)'> " +
-                "<img src=" + pathtoroot + "'copy.svg' alt='" + messages.copyUrl + "'> " +
+                "<img src='" + pathtoroot + "copy.svg' alt='" + messages.copyUrl + "'> " +
                 "<span data-copied='" + messages.urlCopied + "'></span></button>"));
         },
         function () {
